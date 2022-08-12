3d012a9f17c75dcfc61c2376df0d08e8bcfb2673
sury2317 <surya.sachidhanandamurthi@chainsys.com>
2022-08-12 21:18:25 +0530

    Committed for error console

--- routes/classRoutesApi.js
+++ routes/classRoutesApi.js
@@ -24,7 +24,7 @@ router.get('/classes', async(req, res) => {
             "response":findResult
         })   
     } catch (error) {
-        console.log('Error ==>',error);
+        console.log('Error in getting the docs==>',error);
         res.send({'status':'Failure in getting the docs'})
     } finally {
         client.close();
@@ -47,7 +47,7 @@ router.get('/classes/:id', async(req, res) => {
             "response":findResult
         })   
     } catch (error) {
-        console.log('Error ==>',error);
+        console.log('Error in getting the doc==>',error);
         res.send({'status':'Failure in getting the doc'})
     } finally {
         client.close();
@@ -67,7 +67,7 @@ router.post('/classes/insert', async(req, res) => {
             "response":insertResult
         })   
     } catch (error) {
-        console.log('Error ==>',error);
+        console.log('Error in inserting the doc==>',error);
         res.send({'status':'Failure in inserting the doc'})
     } finally {
         client.close();
@@ -91,7 +91,7 @@ router.put('/classes/:classId/:section', async (req, res) => {
             "response":updatedResult
         })
     } catch (error) {
-        console.log('Error ==>',error);
+        console.log('Error in updating the doc==>',error);
         res.send({'status':'Failure in updating the doc'})
     } finally {
         client.close();
@@ -113,8 +113,8 @@ router.delete('/classes/:class', async (req, res) => {
             "response":deleteResult
         })
     } catch (error) {
-        console.log('Error ==>',error);
-        res.send({'status':'Failure in updating the doc'})
+        console.log('Error in deleting the doc ==>',error);
+        res.send({'status':'Failure in deleting the doc'})
     } finally {
         client.close()
     }
